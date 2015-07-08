7a0787e48fa7d453eaf15b9e53ea92140e0c0d3c
Amina MARIE <amarie@amabis.fr>
2015-07-08 11:37:13 +0200

    update finder cppunit

--- FindCppunit.cmake
+++ FindCppunit.cmake
@@ -1,49 +1,75 @@
-# - Find cppunit
-# based on a version found at:
-# https://id.eesti.ee/trac/browser/cmake/modules/FindCppUnit.cmake
+# - try to find cppunit library
 #
-# Find the native cppunit includes and library
+# Cache Variables: (probably not for direct use in your scripts)
+#  CPPUNIT_INCLUDE_DIR
+#  CPPUNIT_LIBRARY
 #
-#  CPPUNIT_INCLUDE_DIR - where to find cppunit/Test.h, etc.
-#  CPPUNIT_LIBRARIES   - List of libraries when using cppunit.
-#  CPPUNIT_FOUND       - True if cppunit found.
-#  CPPUNIT_DEFINITIONS - You should add_definitons(${LIBSMI_DEFINITIONS}) before
-#                       compiling code that includes cppunit library files.
+# Non-cache variables you might use in your CMakeLists.txt:
+#  CPPUNIT_FOUND
+#  CPPUNIT_INCLUDE_DIRS
+#  CPPUNIT_LIBRARIES
+#
+# Requires these CMake modules:
+#  SelectLibraryConfigurations (included with CMake >= 2.8.0)
+#  FindPackageHandleStandardArgs (known included with CMake >=2.6.2)
+#
+# Original Author:
+# 2009-2011 Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net>
+# http://academic.cleardefinition.com
+# Iowa State University HCI Graduate Program/VRAC
+#
+# Copyright Iowa State University 2009-2011.
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
 
-IF (CPPUNIT_INCLUDE_DIR)
-  # Already in cache, be silent
-  SET(CPPUNIT_FIND_QUIETLY TRUE)
-ENDIF (CPPUNIT_INCLUDE_DIR)
+set(CPPUNIT_ROOT_DIR
+  "${CPPUNIT_ROOT_DIR}"
+  CACHE
+  PATH
+  "Directory to search")
 
-INCLUDE(FindPkgConfig)
-IF (PKG_CONFIG_FOUND)
-  IF (CPPUNIT_FIND_REQUIRED AND CPPUNIT_FIND_VERSION)
-    SET(_PACKAGE_ARGS cppunit>=${CPPUNIT_FIND_VERSION} REQUIRED)
-  ELSE ()
-    SET(_PACKAGE_ARGS cppunit)
-  ENDIF ()
-  IF( NOT CPPUNIT_FIND_QUIETLY)
-    MESSAGE("XXX use pkg-config --modversion ${_PACKAGE_ARGS} ...")
-  ENDIF( NOT CPPUNIT_FIND_QUIETLY)
-  PKG_CHECK_MODULES(CPPUNIT ${_PACKAGE_ARGS})
-ENDIF (PKG_CONFIG_FOUND)
-SET(CPPUNIT_DEFINITIONS ${CPPUNIT_CFLAGS_OTHER})
+find_library(CPPUNIT_LIBRARY_RELEASE
+  NAMES
+  cppunit
+  HINTS
+  "${CPPUNIT_ROOT_DIR}")
 
-FIND_PATH(CPPUNIT_INCLUDE_DIR cppunit/Test.h HINTS ${CPPUNIT_INCLUDE_DIRS})
+find_library(CPPUNIT_LIBRARY_DEBUG
+  NAMES
+  cppunitd
+  HINTS
+  "${CPPUNIT_ROOT_DIR}")
 
-SET(CPPUNIT_NAMES cppunit)
-FIND_LIBRARY(CPPUNIT_LIBRARY NAMES ${CPPUNIT_NAMES} HINTS ${CPPUNIT_LIBRARY_DIRS} )
+include(SelectLibraryConfigurations)
+select_library_configurations(CPPUNIT)
 
-IF (NOT PKG_CONFIG_FOUND)
-  SET(CPPUNIT_LIBRARIES ${CPPUNIT_NAMES})
-  GET_FILENAME_COMPONENT(CPPUNIT_LIBRARY_DIRS ${CPPUNIT_LIBRARY} PATH)
-ENDIF (NOT PKG_CONFIG_FOUND)
+# Might want to look close to the library first for the includes.
+get_filename_component(_libdir "${CPPUNIT_LIBRARY_RELEASE}" PATH)
 
-# handle the QUIETLY and REQUIRED arguments and set CPPUNIT_FOUND to TRUE if
-# all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(CppUnit DEFAULT_MSG CPPUNIT_LIBRARY
-    CPPUNIT_INCLUDE_DIR CPPUNIT_LIBRARIES CPPUNIT_LIBRARY_DIRS)
+find_path(CPPUNIT_INCLUDE_DIR
+  NAMES
+  cppunit/TestCase.h
+  HINTS
+  "${_libdir}/.."
+  PATHS
+  "${CPPUNIT_ROOT_DIR}"
+  PATH_SUFFIXES
+  include/)
 
-MARK_AS_ADVANCED( CPPUNIT_LIBRARY CPPUNIT_INCLUDE_DIR CPPUNIT_LIBRARIES
-    CPPUNIT_LIBRARY_DIRS CPPUNIT_DEFINITIONS )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(cppunit
+  DEFAULT_MSG
+  CPPUNIT_LIBRARY
+  CPPUNIT_INCLUDE_DIR)
+
+if(CPPUNIT_FOUND)
+  set(CPPUNIT_LIBRARIES ${CPPUNIT_LIBRARY} ${CMAKE_DL_LIBS})
+  set(CPPUNIT_INCLUDE_DIRS "${CPPUNIT_INCLUDE_DIR}")
+  mark_as_advanced(CPPUNIT_ROOT_DIR)
+endif()
+
+mark_as_advanced(CPPUNIT_INCLUDE_DIR
+  CPPUNIT_LIBRARY_RELEASE
+  CPPUNIT_LIBRARY_DEBUG)
